680a6689b81efed58500cfda241f61792585ec43
Admin <Admin@OFFICE15>
2023-03-06 11:13:37 +0400

    Добавьте файлы проекта.

---
+++ StringIndexReplyLetter/StringIndexReplyLetter.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	const int size = 30;
+
+	cout << "Введите текст: " << endl;
+	char buffer[size]{};
+	gets_s(buffer);
+
+	cout << "Укажите букву, позиции которых нужно найти в тексте: " << endl;
+	char letter{};
+	cin >> letter;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (*(buffer + i) == letter)
+		{
+			cout << "Символ обнаружен в индексе: " << i + 1 << endl;
+		}
+	}
+
+	return 0;
+}
